a66a02ffb5fc1f3f3b264d8cb9c4a59a2bef7496
YerardinPerlaza <yerardinpm@gmail.com>
2021-04-03 16:30:04 +0000

    pathperline

--- _pathperline.c
+++ _pathperline.c
@@ -1,54 +1,21 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-int _strncmp(char *s1, char *s2, int n);
-int _strlen(char *s);
-/**
- * main - prints the environment
- *
- * Return: Always 0.
- */
-char *_getenv(const char *name)
-{
-	int len = _strlen((char *)name);
-	extern char **environ;
-        char **env = environ;
+#include <unistd.h>
+#include <string.h>
 
-	while (*env != NULL)
-	{
-		if (_strncmp(*env, (char *)name, len) == 0 && env[0][len] == '=')
-			return (*env + len + 1);
-		env++;
-	}
-	return (NULL);
-}
-int _strncmp(char *s1, char *s2, int n)
+int main()
 {
-	int i = 0;
-	while (i < n)
+	char *path;
+	char *tok;
+	int i;
+
+	path = getenv("PATH");
+	tok = strtok(path, "/");
+	for (i = 0; tok != NULL; i++)
 	{
-		if (s1[i] != s2[i])
-			return (-1);
-		else
-			i++;
+		printf("/%s\n", tok);
+		tok = strtok(NULL, "/");
 	}
+	tok = NULL;
 	return (0);
-}
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (*s != '\0')
-	{
-		l++;
-		s++;
-	}
-	return (l);
-}
-int main(void)
-{
-        printf("real getenv: %s\n", getenv("PATH"));
-        printf("my getenv: %s\n", _getenv("PATH"));
-        return (0);
 }
