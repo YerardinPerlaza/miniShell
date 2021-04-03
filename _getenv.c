3100942a836373cab6378afedcf665b10ad38c3c
YerardinPerlaza <yerardinpm@gmail.com>
2021-04-03 15:58:53 +0000

    _getenv

---
+++ _getenv.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int _strncmp(char *s1, char *s2, int n);
+int _strlen(char *s);
+/**
+ * main - prints the environment
+ *
+ * Return: Always 0.
+ */
+char *_getenv(const char *name)
+{
+	int len = _strlen((char *)name);
+	char **env;
+
+	while (*env != NULL)
+	{
+		if (_strncmp(*env, (char *)name, len) == 0 && env[0][len] == '=')
+			return (*env + len + 1);
+		env++;
+	}
+	return (NULL);
+}
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return (-1);
+		else
+			i++;
+	}
+	return (0);
+}
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
